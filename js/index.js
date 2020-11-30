8eecd606abced04955e99a9e71dbcd6835cb605a
abcd1221-12 <2744172618@qq.com>
2020-11-30 10:45:04 +0800

    first

---
+++ js/index.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    var mySwiper = new Swiper ('.swiper-container', {
+        // direction: 'vertical', // 垂直切换选项 方向 默认水平
+        loop: true, // 循环模式选项
+        autoplay: true,
+        parallax : true,
+        
+        // 如果需要分页器
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        
+        // 如果需要前进后退按钮
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        
+        // 如果需要滚动条
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      })  
+}
+ 
